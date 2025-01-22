754737eee3dd0f9d58575598b421fd31c24c5e1d
Карпенко Артем <artem.karpenko@gazprombank.ru>
2025-01-23 00:28:18 +0300

    Техническое задание Финальный проект спринта 6

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -6,42 +6,44 @@ import java.util.List;
 public class Epic extends Task {
     private final List<Subtask> subtasks;
 
-    public Epic(String name,String description){
-        super(name,description);
+    public Epic(String name, String description) {
+        super(name, description);
         this.subtasks = new ArrayList<>();
     }
-    public List<Subtask> getSubtasks(){
+
+    public List<Subtask> getSubtasks() {
         return subtasks;
     }
-    public void addSubtask(Subtask subtask){
+
+    public void addSubtask(Subtask subtask) {
         subtasks.add(subtask);
         updateStatus();
     }
-    public void removeSubtask(Subtask subtask){
+
+    public void removeSubtask(Subtask subtask) {
         subtasks.remove(subtask);
         updateStatus();
     }
 
-    public void updateStatus(){
-        if(subtasks.isEmpty()){
+    public void updateStatus() {
+        if (subtasks.isEmpty()) {
             setStatus(TaskStatus.NEW);
-        return;
+            return;
         }
 
         boolean allNew = subtasks.stream().allMatch(subtask -> subtask.getStatus() == TaskStatus.NEW);
         boolean allDone = subtasks.stream().allMatch(subtask -> subtask.getStatus() == TaskStatus.DONE);
 
-        if(allNew){
+        if (allNew) {
             setStatus(TaskStatus.NEW);
-        }
-        else if(allDone){
+        } else if (allDone) {
             setStatus(TaskStatus.DONE);
-        }
-        else {
+        } else {
             setStatus(TaskStatus.IN_PROGRESSS);
         }
     }
-    public void clearSubtasks(){
+
+    public void clearSubtasks() {
         subtasks.clear();
     }
 }
